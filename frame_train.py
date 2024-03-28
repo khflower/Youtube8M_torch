0df2a70be382cf3971b51e5baed56667f1d33946
Kanghyeon Kim <105573554+khflower@users.noreply.github.com>
2024-03-28 23:05:23 +0900

    Add files via upload

--- frame_train.py
+++ frame_train.py
@@ -23,6 +23,7 @@ model_save_path = "yt8m_ckpt/frame_model.ckpt"
 
 # 모델 학습
 num_epochs = 30
+save_batch_interval = 100 # 100개 배치마다 저장
 for epoch in range(num_epochs):
     total_loss = 0
     for i, batch in enumerate(dataloader):
@@ -40,6 +41,14 @@ for epoch in range(num_epochs):
         total_loss += loss.item()
         print("loss : ", loss.item())
         print("hit1 : ", calculate_hit_at_one(torch.sigmoid(outputs), labels))
+
+        # 일정 배치마다 모델 저장
+        if (i + 1) % save_batch_interval == 0:
+            save_path = f"1dcnn_epoch{epoch+1}_batch{i+1}.ckpt"
+            torch.save(model.state_dict(), save_path)
+            print(f"Model saved to {save_path}")
+
+
 
     # 에폭마다 손실 출력
     print(f"Epoch {epoch+1}, Loss: {total_loss}")
